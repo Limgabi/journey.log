c675b1465e6abe42802b5a07c448885b9dd810c2
gabi <70315572+Limgabi@users.noreply.github.com>
2024-06-23 12:27:11 +0900

    feat #6 : 선택한 행정 구역 좌표값 조회

--- src/app/_components/search-place/index.tsx
+++ src/app/_components/search-place/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as S from './index.style';
 import { useGetDistrict } from '@/app/_api/search';
 
@@ -20,57 +20,56 @@ interface DistrictDropdown {
 
 export default function SearchPlace() {
 	const [searchText, setSearchText] = useState('');
-
 	const [params, setParams] = useState<{ data: string; attrFilter?: string }>();
 	const [dropdown, setDropdown] = useState<{ name: string; code: string }[]>([]);
-
-	const [adsido, setAdsodi] = useState({ name: '', code: '' });
+	const [adsido, setAdsido] = useState({ name: '', code: '' });
 	const [adsigg, setAdsigg] = useState({ name: '', code: '' });
 	const [ademd, setAdemd] = useState({ name: '', code: '' });
 	const [adri, setAdri] = useState({ name: '', code: '' });
 
+	const [isGoogleApiLoaded, setIsGoogleApiLoaded] = useState(false);
+	const [coords, setCoords] = useState({ lat: 0, lng: 0 });
+
 	const { data } = useGetDistrict(params);
 
 	const handleClickAd = (data: { name: string; code: string }) => {
-		if (!!!adsido.code && !!!adsigg.code && !!!ademd.code && !!!adri.code) {
-			setAdsodi(data);
-		}
-		if (!!adsido.code && !!!adsigg.code && !!!ademd.code && !!!adri.code) {
+		if (!adsido.code && !adsigg.code && !ademd.code && !adri.code) {
+			setAdsido(data);
+		} else if (adsido.code && !adsigg.code && !ademd.code && !adri.code) {
 			setAdsigg(data);
-		} else if (!!adsido.code && !!adsigg.code && !!!ademd.code && !!!adri.code) {
+		} else if (adsido.code && adsigg.code && !ademd.code && !adri.code) {
 			setAdemd(data);
-		} else if (!!adsido.code && !!adsigg.code && !!ademd.code && !!!adri.code) {
+		} else if (adsido.code && adsigg.code && ademd.code && !adri.code) {
 			setAdri(data);
 		}
 	};
 
 	useEffect(() => {
-		let dropdownData = [];
+		let dropdownData: { name: string; code: string }[] = [];
 
-		if (!!!adsido.code && !!!adsigg.code && !!!ademd.code && !!!adri.code) {
+		if (!adsido.code && !adsigg.code && !ademd.code && !adri.code) {
 			dropdownData =
 				data?.map((e: DistrictDropdown) => ({
-					name: e.properties.ctp_kor_nm,
-					code: e.properties.ctprvn_cd,
+					name: e.properties.ctp_kor_nm || '',
+					code: e.properties.ctprvn_cd || '',
 				})) || [];
-		}
-		if (!!adsido.code && !!!adsigg.code && !!!ademd.code && !!!adri.code) {
+		} else if (adsido.code && !adsigg.code && !ademd.code && !adri.code) {
 			dropdownData =
 				data?.map((e: DistrictDropdown) => ({
-					name: e.properties.sig_kor_nm,
-					code: e.properties.sig_cd,
+					name: e.properties.sig_kor_nm || '',
+					code: e.properties.sig_cd || '',
 				})) || [];
-		} else if (!!adsido.code && !!adsigg.code && !!!ademd.code && !!!adri.code) {
+		} else if (adsido.code && adsigg.code && !ademd.code && !adri.code) {
 			dropdownData =
 				data?.map((e: DistrictDropdown) => ({
-					name: e.properties.emd_kor_nm,
-					code: e.properties.emd_cd,
+					name: e.properties.emd_kor_nm || '',
+					code: e.properties.emd_cd || '',
 				})) || [];
-		} else if (!!adsido.code && !!adsigg.code && !!ademd.code && !!!adri.code) {
+		} else if (adsido.code && adsigg.code && ademd.code && !adri.code) {
 			dropdownData =
 				data?.map((e: DistrictDropdown) => ({
-					name: e.properties.li_kor_nm,
-					code: e.properties.li_cd,
+					name: e.properties.li_kor_nm || '',
+					code: e.properties.li_cd || '',
 				})) || [];
 		}
 
@@ -78,22 +77,74 @@ export default function SearchPlace() {
 	}, [data, adsido, adsigg, ademd, adri]);
 
 	useEffect(() => {
-		if (!!!adsido.code && !!!adsigg.code && !!!ademd.code && !!!adri.code) {
-			setParams({ data: 'LT_C_ADSIDO_INFO' });
+		let dataParam: string = '';
+		let attrFilterParam: string | undefined = undefined;
+
+		if (!adsido.code && !adsigg.code && !ademd.code && !adri.code) {
+			dataParam = 'LT_C_ADSIDO_INFO';
+		} else if (adsido.code && !adsigg.code && !ademd.code && !adri.code) {
+			dataParam = 'LT_C_ADSIGG_INFO';
+			attrFilterParam = `sig_cd:like:${adsido.code}`;
+		} else if (adsido.code && adsigg.code && !ademd.code && !adri.code) {
+			dataParam = 'LT_C_ADEMD_INFO';
+			attrFilterParam = `emd_cd:like:${adsigg.code}`;
+		} else if (adsido.code && adsigg.code && ademd.code && !adri.code) {
+			dataParam = 'LT_C_ADRI_INFO';
+			attrFilterParam = `li_cd:like:${ademd.code}`;
 		}
-		if (!!adsido.code && !!!adsigg.code && !!!ademd.code && !!!adri.code) {
-			setParams({ data: 'LT_C_ADSIGG_INFO', attrFilter: `sig_cd:like:${adsido.code}` });
-		} else if (!!adsido.code && !!adsigg.code && !!!ademd.code && !!!adri.code) {
-			setParams({ data: 'LT_C_ADEMD_INFO', attrFilter: `emd_cd:like:${adsigg.code}` });
-		} else if (!!adsido.code && !!adsigg.code && !!ademd.code && !!!adri.code) {
-			setParams({ data: 'LT_C_ADRI_INFO', attrFilter: `li_cd:like:${ademd.code}` });
-		}
+
+		setParams({ data: dataParam, attrFilter: attrFilterParam });
 	}, [adsido, adsigg, ademd, adri]);
+
+	useEffect(() => {
+		const loadGoogleMapsScript = () => {
+			const script = document.createElement('script');
+			script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}&libraries=places&callback=initMap`;
+			script.async = true;
+			script.defer = true;
+			window.initMap = () => setIsGoogleApiLoaded(true);
+			document.head.appendChild(script);
+		};
+
+		if (!window.google) {
+			loadGoogleMapsScript();
+		} else {
+			setIsGoogleApiLoaded(true);
+		}
+	}, []);
+
+	const geocode = (address: string) => {
+		if (!isGoogleApiLoaded) {
+			return;
+		}
+
+		const geocoder = new window.google.maps.Geocoder();
+
+		geocoder.geocode({ address }, (results: any, status: string) => {
+			if (status === 'OK') {
+				const location = results[0].geometry.location;
+				setCoords({
+					lat: location.lat(),
+					lng: location.lng(),
+				});
+			} else {
+				console.error(status);
+			}
+		});
+	};
+
+	const handleSearch = () => {
+		geocode(`${adsido.name} ${adsigg.name} ${ademd.name} ${adri.name}`);
+	};
 
 	return (
 		<div>
 			<S.InputWrapper>
+				<div>{`${adsido.name} ${adsigg.name} ${ademd.name} ${adri.name}`}</div>
 				<input type="text" value={searchText} onChange={e => setSearchText(e.target.value)} />
+				<button onClick={handleSearch} disabled={!isGoogleApiLoaded}>
+					Search
+				</button>
 			</S.InputWrapper>
 			<ul>
 				{dropdown.map((data, idx) => (
