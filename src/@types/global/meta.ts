5cc9382870dce21c0f4fe8879a8aadfb34e2d4e8
gabi <70315572+Limgabi@users.noreply.github.com>
2024-06-29 13:56:23 +0900

    refactor #20 : 키워드 검색 api 호출을 react query로 변경

---
+++ src/@types/global/meta.ts
@@ -0,0 +1,14 @@
+import { QueryKey, UseMutationOptions, UseQueryOptions } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+
+export type ResponseError = AxiosError<{}>;
+
+export type UseQueryCustomOptions<TQueryFnData = unknown, TData = TQueryFnData> = Omit<
+	UseQueryOptions<TQueryFnData, ResponseError, TData, QueryKey>,
+	'queryKey'
+>;
+
+export type UseMutationCustomOptions<TData = unknown, TVariables = unknown> = Omit<
+	UseMutationOptions<TData, ResponseError, TVariables, unknown>,
+	'mutationFn'
+>;
